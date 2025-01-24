9e8721c287351db6a505bd37490f3924cf078252
Piotr Równicki <piotr.rownicki@gearcode.eu>
2025-01-24 16:04:02 +0100

    Dodano podstawowy projekt

---
+++ smart_nest/lib/styles/styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeAutomotionStyle{
+  static const TextStyle elevatedButtonTextStyle = TextStyle(
+    color: Colors.black, fontWeight: FontWeight.bold
+  );
+
+  static const EdgeInsets xsmallPadding = EdgeInsets.all(4);
+  static const EdgeInsets smallPadding = EdgeInsets.all(8);
+  static const EdgeInsets mediumPadding = EdgeInsets.all(16);
+  static const EdgeInsets largePadding = EdgeInsets.all(32);
+  static const EdgeInsets xLargePadding = EdgeInsets.all(64);
+  static const EdgeInsets xxLargePadding = EdgeInsets.all(128);
+
+  static const double smallIconSuze = 16;
+}
